15dc0c9dd8ef4102402e3d38200bbf6962a089a4
Gvatari <lexa7.03.1994@yandex.ru>
2023-03-07 09:18:55 +0700

    Tree start

---
+++ src/components/Tree.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="tree_container">
+        <div class="tree_parent" v-for="TreeElement in TreeElements" :key="TreeElement.value">
+            <div class="tree_name">{{ TreeElement.title }}</div>
+            <div class="tree_children">
+                <tree :TreeElements="TreeElement.childrenElements"></tree>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'tree',
+props: ['TreeElements'],
+data() {
+    return {
+       
+    }
+},
+}
+</script>
+
+<style scoped>
+.tree_container {
+    width: 300px;
+    max-height: 582px;
+    display: flex;
+    flex-direction: column;
+}
+.tree_parent {
+    padding-left: 4px;
+}
+.tree_name {
+    height: 36px;
+    display: flex;
+    align-items: center;
+}
+</style>
--- src/views/turns/year.vue
+++ src/views/turns/year.vue
@@ -1,17 +1,52 @@
 <template>
     <div>
         <TitlePages general_message="Выпуск продукции" private_message="2022г." />
+        <tree class="tree" :TreeElements="TreeElements"></tree>
     </div>
 </template>
  
 <script>
 import TitlePages from '@/components/TitlePages.vue'
+import Tree from '@/components/Tree.vue'
 export default {
     name: 'year',
     components: {
-        TitlePages
-    }
+        TitlePages,
+        Tree
+    },
+    data() {
+        return {
+            TreeElements: [
+                {
+                    title: 'Альт-Пласт',
+                    childrenElements: [
+                        {
+                            title: 'Готовая продукция',
+                            childrenElements: [
+                                {
+                                    title: 'Кухня',
+                                    childrenElements: []
+                                }
+                            ]
+                        },
+                        {
+                            title: 'Произведённые детали',
+                            childrenElements: []
+                        },
+                    ]
+                },
+                {
+                    title: 'ИП-Панков',
+                    childrenElements: {}
+                }
+            ]
+        }
+    },
 }
 </script>
  
-<style></style>
+<style scoped>
+.tree {
+    margin: 0 20px;
+}
+</style>
